8166d601decf1eb4e7e52a8eaf92ce77f893fa20
VIVEK THUMU <thumuvivek2003@gmail.com>
2025-05-16 20:03:49 +0530

    update as it redirect to property

--- src/pages/MapView/locations.js
+++ src/pages/MapView/locations.js
@@ -4,7 +4,7 @@ const locations = [
       name: "Property A",
       description: "A beautiful place to live.",
       position: [51.505, -0.09],
-      link: "?id=1",
+      link: "/property/1",
       address: "10 Southbank, London SE1 7SG, UK",
       propertySize: 80,
     },
@@ -13,7 +13,7 @@ const locations = [
       name: "Property B",
       description: "A luxury estate.",
       position: [51.51, -0.1],
-      link: "?id=2",
+      link: "/property/2",
       address: "25 Blackfriars Road, London SE1 8NY, UK",
       propertySize: 80,
     }
